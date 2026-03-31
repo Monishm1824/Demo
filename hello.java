6f5f6a271e2c67a812ea623bbbda124b06d6d87d
Monishm1824 <monishwaran2005@gmail.com>
2026-03-31 12:48:38 +0530

    file two

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class rectangle
+{
+	static Scanner sc=new Scanner(System.in)
+	public static void main(String[] args) 
+	{
+		System.out.print("enter the length of the rectangle :");
+		int l= sc.nextInt();
+		System.out.print("enter the width of the rectangle :");
+	}
+}
